e89917f6dcba19a2450578ce1ac7232ed9f763eb
yo16 <xxyo1xx@gmail.com>
2018-11-30 00:29:49 +0900

    fixed #9. Add pagination.

--- v_booklist.py
+++ v_booklist.py
@@ -10,17 +10,40 @@ from flask import Flask, render_template, request
 from models import Books, Book, Tag, Publisher
 from google.appengine.ext import ndb
 
+# １ページの表示件数
+NUM_OF_LIST = 10
+
 
 def main():
-    # Get infos from Datastore.
-    #bs = get_books()
-    #print "----------"
+    # Datastoreから情報を取得
     q = Book.query()
-    #q = ndb.Query(Book)
-    print q
     bs_db = q.fetch()
-    #print bs
-    #print "type", type(bs)
+
+    # 表示するページを決める
+    cur_page = 0
+    last_page = int(len(bs_db) / NUM_OF_LIST)
+    if 'page' in request.args:
+        str_param_page = request.args.get('page')
+        if str_param_page.isdecimal():
+            cur_page = int(str_param_page)
+            if cur_page < 0:
+                cur_page = 0
+    if len(bs_db) <= cur_page * NUM_OF_LIST:
+        # データ量を超えたページ指定の場合は最終ページ
+        cur_page = last_page
+    # 前のページ、次のページ番号も決める
+    prev_page = -1
+    if 0 < cur_page:
+        prev_page = cur_page - 1
+    next_page = -1
+    if cur_page < last_page:
+        next_page = cur_page + 1
+
+    # fetchした結果を、ページングするデータだけに絞る
+    start_item = cur_page * NUM_OF_LIST
+    end_item = start_item + NUM_OF_LIST - 1
+    bs_db = bs_db[start_item : end_item+1]
+
     bs = []
     for b in bs_db:
         cur_book = {}
@@ -41,9 +64,19 @@ def main():
             cur_book['tags'].append(t.tag_name)
         bs.append(cur_book)
     
+    # ページ情報
+    page_info = {
+        'prev': prev_page,
+        'cur': cur_page,
+        'next': next_page,
+        'last': last_page
+    }
+    #print(page_info)
+
     return render_template(
         'booklist.html',
-        books=bs
+        books=bs,
+        page=page_info
     )
 
 def get_books():
