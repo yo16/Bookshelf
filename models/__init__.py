9472971f91cc8d65aa54d0cf1fa4a91fc29af085
yo16 <xxyo1xx@gmail.com>
2018-12-01 23:31:13 +0900

    fixed #25

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+
+from .clsBook import Book
+from .clsBooks import Books
+from .clsPublisher import Publisher
+from .clsTag import Tag
+
---
+++ models/clsTag.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from google.appengine.ext import ndb
+
+
+class Tag(ndb.Model):
+    """ Tag Model class
+    kind['Tag'] - entity
+    """
+    tag_name = ndb.StringProperty()
+
+    @staticmethod
+    def regist_tag(tag_name_):
+        """タグを登録または更新する
+        
+        Args:
+            tag_name_ (str): タグの文字列
+        
+        Returns:
+            (int): 登録 or 更新されたTagのkey_id
+        """
+        t = Tag.query(Tag.tag_name==tag_name_).get()
+        tag_key = None
+        if t is None:
+            # 登録
+            reg_tag = {'tag_name': tag_name_}
+            tag_key = Tag(**reg_tag).put()
+        else:
+            # あったらキーを返す
+            tag_key = t.key
+        
+        return tag_key
+    
+
+    @staticmethod
+    def get_tag_by_id(key_id):
+        """get_tag_by_id
+        DatastoreのキーIDから、Tagを得る
+
+        Args:
+            key_id (str): Tagのkey_id
+        
+        Returns:
+            (str): タグ名
+        """
+        t_key = ndb.Key(Tag, key_id)
+        if t_key is None:
+            return None
+        
+        return t_key.get()
+    
